3ccdf853ed2462fa9d3952085b9fbfbc6cb93699
soorajpky <soorajpky5@gmail.com>
2025-05-12 13:37:13 +0530

    Updated hoarding form with showroom dropdowns and UI improvements

--- forms.py
+++ forms.py
@@ -1,5 +1,5 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, SubmitField, FloatField, DateField
+from wtforms import StringField, PasswordField, SubmitField, FloatField, DateField, SelectField
 from flask_wtf.file import FileField, FileAllowed
 from wtforms.validators import DataRequired
 
@@ -17,9 +17,24 @@ class HoardingForm(FlaskForm):
     contact           = StringField("Contact No.", validators=[DataRequired()])
     address           = StringField("Address", validators=[DataRequired()])
     location_url      = StringField("Google Maps URL", validators=[DataRequired()])
-    showroom_name     = StringField("Showroom Name")  # used via request.form
-    showroom_location = StringField("Showroom Location")  # used via request.form
-    image             = FileField("Hoarding Image", validators=[
-                          FileAllowed(['jpg', 'png', 'jpeg', 'gif'], 'Images only!')
-                      ])
+    
+    showroom_name     = SelectField("Showroom Name", choices=[
+        ("Rajakumari Gold and Diamonds", "Rajakumari Gold and Diamonds"),
+        ("Rajakumari Silks and Designs", "Rajakumari Silks and Designs"),
+        ("Rajakumari Hypermarket", "Rajakumari Hypermarket")
+    ], validators=[DataRequired()])
+
+    showroom_location = SelectField("Showroom Location", choices=[
+        ("Trivandrum", "Trivandrum"),
+        ("Kaliyikavila", "Kaliyikavila"),
+        ("Attingal", "Attingal"),
+        ("Kallambalam", "Kallambalam"),
+        ("Pothencode", "Pothencode"),
+        ("Kattakada", "Kattakada"),
+        ("Paripally", "Paripally"),
+        ("Kottiyam", "Kottiyam"),
+        ("Trissur", "Trissur")
+    ], validators=[DataRequired()])
+
+    image             = FileField("Hoarding Image", validators=[FileAllowed(['jpg','png','jpeg','gif'])])
     submit            = SubmitField("Save")
